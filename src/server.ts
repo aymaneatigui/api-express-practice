221e3a20b6799f854fbaec86e2368a057e7df25e
aymane <atigui.cs@gmail.com>
2023-11-01 16:59:36 +0100

    config TypeScript

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  console.log("Express Work fine");
   res.status(200);
   res.json({ message: "this is data" });
 });
